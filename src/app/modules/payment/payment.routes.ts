291d8efa1a77a389e7340bad41e697000cbff7bc
Rifatinfo <mdrifathossainsinfo@gmail.com>
2026-01-22 20:42:23 +0600

    add proxi

--- src/app/modules/payment/payment.routes.ts
+++ src/app/modules/payment/payment.routes.ts
@@ -8,5 +8,6 @@ router.post("/init-payment/:orderId", PaymentController.initPayment);
 router.post("/success", PaymentController.successPayment);
 router.post("/fail", PaymentController.failPayment);
 router.post("/cancel", PaymentController.cancelPayment);
+router.post("/validate-payment", PaymentController.validatePayment);
 
 export const PaymentRoutes = router;
